21ed95e478e1533101ecf14ab0d5a84ff396a28d
Marc Davies <marc.davies@bath.edu>
2024-04-01 16:03:10 +0100

    basic actions done

--- RobotCleanerGame/Main.py
+++ RobotCleanerGame/Main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     g = rCG.Game(3, 3, robot_start=(2, 1))
 
     g.add_token((0, 0), "*")  # Universal bin
-
+    g.add_token((1, 0), "R")  # Red bin
     g.add_token((1, 1), "b")  # Blue item
     g.add_token((2, 2), "r")  # Red item
     g.add_token((2, 0), "m")  # Mess
